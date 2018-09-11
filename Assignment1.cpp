9a3e088d6e0334817eff8d38491a4e07ce580773
kingkong <vuhung2881999@gmail.com>
2018-09-11 18:17:17 +0700

    third command

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -58,7 +58,9 @@ int main(){
         float ketQua = P(x,n);
         cout << "P(x, n) = "<< fixed << ketQua << endl;
 
-        cout << "Tong cac so chan lon hon 5 trong doan ["<< n <<", "<< m <<"] la: " << tongChan(m)-tongChan(n) << endl;
+        cout << "Tong cac so chan lon hon 5 trong doan ["<< n <<", "<< m <<"] la: ";
+        if(n%2==0)  cout << tongChan(m) - tongChan(n) + n << endl;
+        else  cout << tongChan(m) - tongChan(n) << endl;
 
         return 0;
 }
